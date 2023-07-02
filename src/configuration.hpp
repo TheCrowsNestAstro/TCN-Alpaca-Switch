94ff161bea2f775ad1a6ca83fefe566d6c364db9
Cameron Tetford <cameron.tetford+github@gmail.com>
2023-07-02 14:07:06 -0400

    Update configuration.hpp

--- src/configuration.hpp
+++ src/configuration.hpp
@@ -11,22 +11,26 @@
 //#define BOARD BOARD_ESP8266_RELAY_MODULE
 #define BOARD BOARD_OPENASTROPOWERHUB
 
+#if BOARD == BOARD_OPENASTROPOWERHUB
+#define DEVICE_NAME "OpenAstroPowerHub"
+#define DRIVER_INFO "OAPH Alpaca Driver"
+#define UNIQUE_CLIENT_ID "785d8e0c4af346208dcd9186a381d8d6" // https://www.guidgenerator.com/online-guid-generator.aspx
+#define NR_OF_RELAYS 5
+#elif BOARD == BOARD_ESP8266_RELAY_MODULE
 #define DEVICE_NAME "TCN Power Switch"
+#define DRIVER_INFO "TCN ASCOM Alpaca Driver"
+#define UNIQUE_CLIENT_ID "9be62a4558494b638924267778cd0350" // https://www.guidgenerator.com/online-guid-generator.aspx
+#define NR_OF_RELAYS 16
+#endif
+#define DESCRIPTION "Mount ASCOM Switch for controlling power supplies"
+#define DRIVER_VERSION "1.1"
+
 #define DEVICE_TYPE "Switch"
 #define DEVICE_NR 0
-#define UNIQUE_CLIENT_ID "9be62a4558494b638924267778cd0350" // https://www.guidgenerator.com/online-guid-generator.aspx
 
-#define DESCRIPTION "Mount ASCOM Switch for controlling power supplies"
-#define DRIVER_INFO "TCN ASCOM Alpaca Driver"
-#define DRIVER_VERSION "1.1"
 
 #define EEPROM_SIZE 512
 // Zero based
-#if BOARD == BOARD_OPENASTROPOWERHUB
-#define NR_OF_RELAYS 5
-#elif BOARD == BOARD_ESP8266_RELAY_MODULE
-#define NR_OF_RELAYS 16
-#endif
 
 #define RELAY_DATA_PIN 14
 #define RELAY_LATCH_PIN 12
